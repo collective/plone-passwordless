f94d155d57cc9b96be5ed6b4757721c5175446f4
Marwan Atef <marwanatef97@gmail.com>
2022-06-11 11:14:06 +0200

    address reviews

--- src/plone/passwordless/browser/views.py
+++ src/plone/passwordless/browser/views.py
@@ -2,8 +2,5 @@ from Products.CMFPlone.resources import add_resource_on_request
 from Products.Five import BrowserView
 
 class PracticingView(BrowserView):
-
-    def __call__(self):
-        # utility function to add resource to rendered page
-        add_resource_on_request(self.request, 'practicing')
-        return super(PracticingView, self).__call__()
+    pass
+        
