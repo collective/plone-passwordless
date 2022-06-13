6259b82ff2aa9e16161d4028a9cc3e3359161f4f
Andreas Jung <info@zopyx.com>
2022-06-13 13:43:13 +0200

    fine tuning

--- src/plone/passwordless/plugin.py
+++ src/plone/passwordless/plugin.py
@@ -11,7 +11,6 @@ from Products.PluggableAuthService.events import PrincipalCreated
 from Products.PluggableAuthService.interfaces import plugins as pas_interfaces
 from Products.PluggableAuthService.interfaces.authservice import _noroles
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
-from Products.PluggableAuthService.utils import createViewName
 from zope.event import notify
 from zope.interface import implementer
 
@@ -23,6 +22,8 @@ logger = logging.getLogger(__name__)
 tpl_dir = os.path.join(os.path.dirname(__file__), "browser")
 
 _marker = {}
+
+ANNOTATION_ID = '_plone_passwordless'
 
 
 def manage_addPasswordlessPlugin(context, id, title="", RESPONSE=None, **kw):
@@ -60,14 +61,13 @@ class PasswordlessPlugin(BasePlugin):
         self._setId(id)
         self.title = title
         self.plugin_caching = True
-#        self._init_trees()
+        self._init_trees()
 
     def _init_trees(self):
         # (provider_name, provider_userid) -> userid
-        self._userid_by_identityinfo = OOBTree()
-
-        # userid -> userdata
-        self._useridentities_by_userid = OOBTree()
+        tree = getattr(self, ANNOTATION_ID, None)
+        if tree is None:
+            setattr(self, ANNOTATION_ID, OOBTree())
 
     @security.public
     def authenticateCredentials(self, credentials):
@@ -79,26 +79,22 @@ class PasswordlessPlugin(BasePlugin):
         - If the credentials cannot be authenticated, return None.
         """
 
-        print("authenticated")
-        return ("aj", "aj")
         login = credentials.get("login", None)
         password = credentials.get("password", None)
         if login == "aj":
             print("authenticated")
             return "aj", "aj"
-        return None
 
-        if not login or login not in self._useridentities_by_userid:
-            return None
-        identities = self._useridentities_by_userid[login]
-        if identities.check_password(password):
-            return login, login
+        return None
 
     @security.public
     def getRolesForPrincipal(self, principal, request=None):
         """ All users authenticated through this plugin get fixed roles """
-        print("inside roles")
-        return ("Member", "Authenticated")
+
+        if principal == "aj":
+            print("inside roles")
+            return ("Member", "Authenticated")
+        return ()
 
 
     @security.public
